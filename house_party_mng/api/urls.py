7be225c3c4702cd18e5db5212b2cab2b7f0f6b66
ksendzovSL <xendzovslava@yandex.ru>
2022-01-09 16:43:12 +0300

    Routers and CreateView:
    - set routers in React for "home", "create" and "join". Needed to check for mistakes
    - added a view in api for creating a music-room

--- house_party_mng/api/urls.py
+++ house_party_mng/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from api.views import RoomView
+
+from api.views import RoomView, CreateRoomView
 
 
 urlpatterns = [
     path('home/', RoomView.as_view()),
+    path('create_room/', CreateRoomView.as_view()),
 ]
