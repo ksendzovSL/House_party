c058065b5bdfe09f8c11ec780fdc054d36427b84
ksendzovSL <xendzovslava@yandex.ru>
2022-01-09 23:22:31 +0300

    UI:
    - added UI for page to create musical room
    - added logic to send data to back

--- house_party_mng/api/urls.py
+++ house_party_mng/api/urls.py
@@ -5,5 +5,5 @@ from api.views import RoomView, CreateRoomView
 
 urlpatterns = [
     path('home/', RoomView.as_view()),
-    path('create_room/', CreateRoomView.as_view()),
+    path('create-room/', CreateRoomView.as_view()),
 ]
